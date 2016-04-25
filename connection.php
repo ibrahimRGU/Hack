26deeb7e39ba7530cf7fcbc6c45c54de3160509f
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-25 23:21:21 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'b307281dd0861c');
 define('DB_PASSWORD', '6f7d250a');
 define('DB_DATABASE', 'hackerton');
