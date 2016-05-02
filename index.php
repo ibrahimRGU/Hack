e6d22fa459255daeb2c7577dfc768b478f834d82
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-05-02 01:24:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>PHP Login Form with Session</title>
+<title>PHP Login Form with Session*</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 
